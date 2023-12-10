2e39c2b52734ffcb84a44e27d31fcc3b3cdf6f36
DSLZL <q2891362919@163.com>
2023-12-10 16:32:48 +0800

    Site updated: 2023-12-10 16:32:47

---
+++ js/FunnyTitle.js
@@ -0,0 +1,16 @@
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = '只看不做是没用的';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = '这里看不懂看其他也是没用的' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
